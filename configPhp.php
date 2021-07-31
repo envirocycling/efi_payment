5c1157b0f832a577948adc8cb20030e84c44e577
Jayson Tolentino <jayjaytolentino20@gmail.com>
2021-07-31 14:32:49 +0800

    fix generate payment order/printed order/generate batch po

--- configPhp.php
+++ configPhp.php
@@ -11,7 +11,7 @@ if($con->connect_error) {
 	echo $con->connect_error;
 }
 
-// $base_url = 'http://paymentsystem.efi.net.ph/user-login/maker';
+$base_url = 'http://paymentsystem.efi.net.ph/user-login/maker';
 
 // $host = "localhost";
 // $username = "root";
